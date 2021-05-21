e7cfcb9bb1188c3fda9d0acd9ea2fda0590f7616
patryk-brzozowski <patryk.brzozowski@interia.pl>
2021-05-21 13:18:36 +0200

    Project done.

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,7 +1,8 @@
 package pl.coderslab;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -26,16 +27,17 @@ public class TaskManager {
             }
             String check = scr.next();
             if (check.equals("add")) {
-                System.out.println("add");
+                add();
             } else if (check.equals("remove")) {
-                System.out.println("remove");
+                remove();
             } else if (check.equals("list")) {
                 list();
             } else if (check.equals("exit")) {
-                System.out.println(ConsoleColors.RED + "Program zakończył pracę.");
+                exit();
+                System.out.println(ConsoleColors.RED + "The program has finished its work.");
                 break;
             } else {
-                System.out.println("Niepoprawna komenda. Spróbuj jeszcze raz");
+                System.out.println("Wrong commend. Try again");
             }
         }
     }
@@ -64,18 +66,62 @@ public class TaskManager {
 
         for (int i = 0; i < tasks.length; i++) {
             for (int j = 0; j < tasks[i].length; j++) {
-                System.out.print(tasks[i][j] + " ");
+                System.out.print(i + " : " + tasks[i][j] + " ");
             }
             System.out.println();
         }
     }
     public static void add() {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Please add task description");
+        String description = scr.nextLine();
+        System.out.println("Please add task due date in format 'rrrr-mm-dd'");
+        String date = " " + scr.nextLine();
+        System.out.println("Is your task important? (true/false)");
+        String importance;
+        while (true) {
+            importance = " " + scr.nextLine();
+            if (importance.equals(" true") || importance.equals(" false")){
+                break;
+            } else {
+                System.out.println("Please type true or false");
+            }
+        }
+        tasks = Arrays.copyOf(tasks, tasks.length+1);
+        tasks[tasks.length-1] = new String[3];
+        tasks[tasks.length-1][0] = description;
+        tasks[tasks.length-1][1] = date;
+        tasks[tasks.length-1][2] = importance;
+        scr.close();
+        System.out.println("Record was successfully added");
     }
-    public static void delete() {
+    public static void remove() {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Please select number of task to remove");
+        while (!scr.hasNextInt()) {
+            System.out.println("It's not a number. Try again.");
+            scr.nextLine();
+        }
+        int number;
+        while (true) {
+            number = scr.nextInt();
+            if (number>=0 && number<tasks.length) {
+                break;
+            } else {
+                System.out.println("Incorrect argument passed. There is no record with such number.");
+            }
+        }
+        tasks = ArrayUtils.remove(tasks, number);
+        System.out.println("Record was successfully deleted.");
     }
     public static void exit() {
-        Path path = Path.of("tasks.csv");
-
+        try (FileWriter writer = new FileWriter("tasks.csv")) {
+            for (int i = 0; i < tasks.length; i++) {
+                writer.append(String.join(",", tasks[i])+"\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Wystąpił problem z dostępem do pliku tasks.csv");
+        }
     }
 
 }
